83578fb6e8372f98d5dfca541ef9c6aabf49c9cf
devpatel0005 <145092047+devpatel0005@users.noreply.github.com>
2024-04-10 12:17:56 +0530

    creating a class atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -1,3 +1,14 @@
 //This project is prepared by dev patel,hari patel,rony padmani
 #include<iostream>
+#include<string>
 using namespace std;
+
+class atm{
+private:										// private member varibles
+	long int account_No;
+	string name;
+	int PIN;
+	double balance;
+	string mobile_No;
+
+};
