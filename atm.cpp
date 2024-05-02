66c2b6c66713db4088d30366c1dd29b3532cc6f2
Hari Patel <148355284+haripatel07@users.noreply.github.com>
2024-05-02 18:10:04 +0530

    Update atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -10,5 +10,27 @@ private:										// private member varibles
 	int PIN;
 	double balance;
 	string mobile_No;
+public:											// public member functions
 
+	// setData function is setting the Data into the private member variables
+	void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a)
+	{
+		account_No = account_No_a;	 // assigning the formal arguments to the private member var's
+		name = name_a;
+		PIN = PIN_a;
+		balance = balance_a;
+		mobile_No = mobile_No_a;
+	}
+
+	//getAccountNo function is returning the user's account no.
+	long int getAccountNo()
+	{
+		return account_No;
+	}
+
+	//getName function is returning the user's Name
+	string getName()
+	{
+		return name;
+	}
 };
